c1eea4c360b4ddef29a305944be7a31012a063f9
Edwjon <edwardpizzurro@gmail.com>
2020-09-29 01:25:13 -0400

    Commit

---
+++ Audible Login Guide/Page.swift
@@ -0,0 +1,15 @@
+//
+//  Page.swift
+//  Audible Login Guide
+//
+//  Created by Edward Pizzurro Fortun on 9/28/20.
+//  Copyright © 2020 Edwjon. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    let title: String
+    let message: String
+    let imageName: String
+}
